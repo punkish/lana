1e0c0882ea239e5487b5b1283ff40e0ef040846d
Sidorela24 <sidiuku1@gmail.com>
2016-12-13 17:30:49 +0100

    Fixed budget.js

---
+++ bin/csv2json.js
@@ -0,0 +1,57 @@
+const node_cj = require("node-csv-json");
+
+node_cj(
+    {
+        input: "/home/sidorela/Projects/lana/data/buxheti/buxheti.csv",
+        output: null
+    },
+    function(err, result){
+        if(err) {
+            console.error(err);
+        }
+        else {
+            //console.log(result)
+            /*
+            { 'Totali buxhetit': '5000',
+    'Programi & Njesite shpenzuese': 'Aparati I Bashkise(Projekte per Mjedisin)',
+    'Shpenzime personeli': '0',
+    'Shpenzime operative': '5000',
+    'Shpenzime per Investime': '0' }
+        */
+          let html = '<table>\n';
+          html += '<tr>';
+
+          result.forEach(function(elem, index) {
+
+            if (index) {
+              html += '<tr>';
+              for (let key in elem) {
+                html += '<td>';
+                html += elem[key];
+                html += '</td>';
+              }
+              html += '</tr>\n';
+            }
+            else {
+              html += '<tr>';
+              for (let key in elem) {
+                html += '<th>';
+                html += key;
+                html += '</th>';
+              }
+              html += '</tr>\n';
+              html += '<tr>';
+              for (let key in elem) {
+                html += '<td>';
+                html += elem[key];
+                html += '</td>';
+              }
+              html += '</tr>\n';
+            }
+
+          });
+          html += '</table>';
+          console.log(html);
+        }
+  }
+);
--- resources/budget.js
+++ resources/budget.js
@@ -1,3 +1,4 @@
+const node_cj = require("node-csv-json");
 const budget = {
     method: 'GET',
 
@@ -5,11 +6,73 @@ const budget = {
 
     handler : function (request, reply) {
 
-        reply.view(
-            'budget', 
-            { data : [] }, 
-            { layout : 'main' }
-        );
+      const node_cj = require("node-csv-json");
+
+      node_cj(
+          {
+              input: "/home/sidorela/Projects/lana/data/buxheti/buxheti.csv",
+              output: null
+          },
+          function(err, result){
+              if(err) {
+                  console.error(err);
+              }
+              else {
+                  //console.log(result)
+                  /*
+                  { 'Totali buxhetit': '5000',
+          'Programi & Njesite shpenzuese': 'Aparati I Bashkise(Projekte per Mjedisin)',
+          'Shpenzime personeli': '0',
+          'Shpenzime operative': '5000',
+          'Shpenzime per Investime': '0' }
+              */
+              /*
+                let html = '<table>\n';
+                html += '<tr>';
+
+                result.forEach(function(elem, index) {
+
+                  if (index) {
+                    html += '<tr>';
+                    for (let key in elem) {
+                      html += '<td>';
+                      html += elem[key];
+                      html += '</td>';
+                    }
+                    html += '</tr>\n';
+                  }
+                  else {
+                    html += '<tr>';
+                    for (let key in elem) {
+                      html += '<th>';
+                      html += key;
+                      html += '</th>';
+                    }
+                    html += '</tr>\n';
+                    html += '<tr>';
+                    for (let key in elem) {
+                      html += '<td>';
+                      html += elem[key];
+                      html += '</td>';
+                    }
+                    html += '</tr>\n';
+                  }
+
+                });
+                html += '</table>';
+                */
+
+                reply.view(
+                    'budget',
+                    { data : result },
+                    { layout : 'main' }
+                );
+              }
+        }
+      );
+
+
+
     },
 
     config : {
